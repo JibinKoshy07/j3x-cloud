d14c875c76dc755b3cf428f84f9f778d0b4fbae8
Jibin Koshy <jibinkoshy07@gmail.com>
2023-07-06 18:28:54 +0530

    Update

---
+++ src/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request,redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def landing():
+    return render_template('landing.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login')
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    
+    # Perform login authentication logic here
+    # You can customize this part to match your specific requirements
+    
+    if username == 'admin' and password == 'password':
+        return redirect('/')
+    
+    else:
+        return 'Login failed!'
+if __name__ == '__main__':
+    app.run()
